be8e13396953fb93d3914d697bec0405befc9236
Manot Luijiu <manot.luijiu@gmail.com>
2018-11-20 23:32:33 +0700

    adding new chapter

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,34 @@
 const gulp = require('gulp');
 const del = require('del');
 const exec = require('child_process').exec;
-var runSequence = require('run-sequence');
 const gulpLoadPlugins = require('gulp-load-plugins');
+
 
 const $ = gulpLoadPlugins();
 
-gulp.task('clean', function() {
+
+gulp.task('clean', function () {
   return del(['docs/**', 'docs/.*', '!docs'], {
     force: true
   });
 });
-gulp.task('copy', function() {
-  return gulp.src(['_book/**/*']).pipe(gulp.dest('docs'));
+gulp.task('copy', function () {
+  return gulp.src([
+      '_book/**/*'
+    ])
+    .pipe(gulp.dest('docs'));
 });
 
-gulp.task('build', function(callback) {
-  return exec('gitbook build', function(err, stdout, stderr) {
+
+gulp.task('build', function (callback) {
+  // In gulp 4, you can return a child process to signal task completion
+  exec('gitbook build', function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
     callback();
   });
 });
 
-gulp.task('default', function(callback) {
-  runSequence('clean', 'build', 'copy');
-});
+gulp.task('default', gulp.series('clean', 'build', 'copy', function (done) {
+  done();
+}));
